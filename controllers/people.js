af8442b4599ff3bb53c9b8b1abffe7ea570c22cb
Nolo Marsh <nolo.marsh@gmail.com>
2021-10-22 14:23:34 -0400

    modified newuser route

--- controllers/people.js
+++ controllers/people.js
@@ -3,30 +3,30 @@ const router = express.Router();
 const postgres = require('../postgres.js');
 
 router.get('/', (req, res) => {
-    postgres.query('SELECT * FROM people ORDER BY id ASC;', (err, results) => {
+    postgres.query('SELECT * FROM person ORDER BY id ASC;', (err, results) => {
         res.json(results.rows)
     });
 });
 
 router.post('/', (req, res) => {
-    postgres.query(`INSERT INTO people (name, age) VALUES ('${req.body.name}', ${req.body.age})`, (err, results) => {
-        postgres.query('SELECT * FROM people ORDER BY id ASC;', (err, results) => {
+    postgres.query(`INSERT INTO person (name, age) VALUES ('${req.body.name}', ${req.body.age})`, (err, results) => {
+        postgres.query('SELECT * FROM person ORDER BY id ASC;', (err, results) => {
             res.json(results.rows)
         });
     })
 });
 
 router.delete('/:id', (req, res) => {
-    postgres.query(`DELETE FROM people WHERE id = ${req.params.id};`, (err, results) => {
-        postgres.query('SELECT * FROM people ORDER BY id ASC;', (err, results) => {
+    postgres.query(`DELETE FROM person WHERE id = ${req.params.id};`, (err, results) => {
+        postgres.query('SELECT * FROM person ORDER BY id ASC;', (err, results) => {
             res.json(results.rows)
         });
     });
 });
 
 router.put('/:id', (req, res) => {
-    postgres.query(`UPDATE people SET name = '${req.body.name}', AGE = ${req.body.age} WHERE id = ${req.params.id}`, (err, results) => {
-        postgres.query('SELECT * FROM people ORDER BY id ASC;', (err, results) => {
+    postgres.query(`UPDATE person SET name = '${req.body.name}', AGE = ${req.body.age} WHERE id = ${req.params.id}`, (err, results) => {
+        postgres.query('SELECT * FROM person ORDER BY id ASC;', (err, results) => {
             res.json(results.rows)
         });
     })
